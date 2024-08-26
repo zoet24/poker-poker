508908d2512e82f8a4bad71f733efedf7d8b7b0e
Zoe Thexton <zoe.thexton@anya.health>
2024-08-26 18:44:09 +0100

    Add new layout to add player

--- src/components/PlayerAdd/index.tsx
+++ src/components/PlayerAdd/index.tsx
@@ -1,30 +1,39 @@
 const PlayerAdd = () => {
+  const addPlayer = () => {
+    console.log("Add player");
+  };
+
   return (
-    <div className="container justify-between">
+    <div className="container cursor-pointer" onClick={addPlayer}>
       <div className="flex">
         <div className="border border-dashed w-20 h-20 rounded-full flex items-center justify-center"></div>
-        <div className="border border-dashed w-10 h-10 rounded-full flex items-center justify-center self-center ml-[-16px] bg-white"></div>
-      </div>
-      <div className="player-cards">
-        <div className="player-card player-card--1">
-          <div className="card border-dashed"></div>
-        </div>
-        <div className="player-card player-card--2">
-          <div className="card border-dashed"></div>
-        </div>
-        <div className="best-card best-card--1">
-          <div className="card card--square border-dashed"></div>
-        </div>
-        <div className="best-card best-card--2">
-          <div className="card card--square border-dashed"></div>
-        </div>
-        <div className="best-card best-card--3">
-          <div className="card card--square border-dashed"></div>
+        <div className="border border-dashed w-10 h-10 rounded-full flex items-center justify-center self-center ml-[-16px] bg-white overflow-hidden">
+          <div className="w-full text-center transform transition-transform duration-500"></div>
         </div>
       </div>
-      <div className="flex flex-col items-center justify-center w-20">
-        <h4>Beep</h4>
-        <p>Boop</p>
+      <div className="overflow-hidden w-full">
+        <div className="flex justify-between transform transition-transform duration-500">
+          <div className="pl-2 player-cards">
+            <div className="player-card player-card--1">
+              <div className="card border-dashed"></div>
+            </div>
+            <div className="player-card player-card--2">
+              <div className="card border-dashed"></div>
+            </div>
+            <div className="best-card best-card--1">
+              <div className="card card--square border-dashed"></div>
+            </div>
+            <div className="best-card best-card--2">
+              <div className="card card--square border-dashed"></div>
+            </div>
+            <div className="best-card best-card--3">
+              <div className="card card--square border-dashed"></div>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center w-20">
+            <p>Add player</p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/PlayerRow/index.tsx
+++ src/components/PlayerRow/index.tsx
@@ -1,3 +1,6 @@
+import LayoutContext from "contexts/LayoutContext";
+import { useContext } from "react";
+
 interface PlayerRowProps {
   name?: string;
   potValue?: number;
@@ -9,36 +12,54 @@ const PlayerRow: React.FC<PlayerRowProps> = ({
   potValue = 10,
   hand = "High Card",
 }) => {
+  const { showCards } = useContext(LayoutContext);
+
   return (
-    <div className="container justify-between">
+    <div className="container">
       <div className="flex">
         <div className="border w-20 h-20 rounded-full flex items-center justify-center">
           {name}
         </div>
-        <div className="border w-10 h-10 rounded-full flex items-center justify-center self-center ml-[-16px] bg-black text-white">
-          {potValue}
+        <div
+          className={`border w-10 h-10 rounded-full flex items-center justify-center self-center ml-[-16px] bg-black text-white overflow-hidden`}
+        >
+          <div
+            className={`w-full text-center transform transition-transform duration-500 ${
+              showCards ? "translate-x-0" : "-translate-x-full"
+            }`}
+          >
+            {potValue}
+          </div>
         </div>
       </div>
-      <div className="player-cards">
-        <div className="player-card player-card--1">
-          <div className="card"></div>
+      <div className="overflow-hidden w-full">
+        <div
+          className={`flex justify-between transform transition-transform duration-500 ${
+            showCards ? "translate-x-0" : "-translate-x-full"
+          }`}
+        >
+          <div className="pl-2 player-cards">
+            <div className="player-card player-card--1">
+              <div className="card"></div>
+            </div>
+            <div className="player-card player-card--2">
+              <div className="card"></div>
+            </div>
+            <div className="best-card best-card--1">
+              <div className="card card--square"></div>
+            </div>
+            <div className="best-card best-card--2">
+              <div className="card card--square"></div>
+            </div>
+            <div className="best-card best-card--3">
+              <div className="card card--square"></div>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center w-20">
+            <h4>{hand}</h4>
+            <p>457</p>
+          </div>
         </div>
-        <div className="player-card player-card--2">
-          <div className="card"></div>
-        </div>
-        <div className="best-card best-card--1">
-          <div className="card card--square"></div>
-        </div>
-        <div className="best-card best-card--2">
-          <div className="card card--square"></div>
-        </div>
-        <div className="best-card best-card--3">
-          <div className="card card--square"></div>
-        </div>
-      </div>
-      <div className="flex flex-col items-center justify-center w-20">
-        <h4>{hand}</h4>
-        <p>457</p>
       </div>
     </div>
   );
